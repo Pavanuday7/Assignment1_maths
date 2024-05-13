608b900a97ab9fe43d6ec57c776550e1649c1e7b
Pavanuday7 <168706915+Pavanuday7@users.noreply.github.com>
2024-05-13 16:58:12 +0530

    Create 16.GreatestOfThreeNumbers.cpp

---
+++ 16.GreatestOfThreeNumbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int greatestOfThree(int A, int B, int C) {
+        // code here
+        if(A>B){
+            if(A>C){
+                return A;
+                
+            }
+            else{
+                return C;
+            }
+        }
+        else{
+            if(B>C){
+                return B;
+            }
+            else{
+                return C;
+            }
+        }
+    }
+};
